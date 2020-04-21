454c06e8f2129b59223c3dcbaf277162e0280d22
Pauline Castets <pauline.castets@gmail.com>
2020-04-21 11:30:57 +0200

    Initial code for the EKS network module

---
+++ variables.tf
@@ -0,0 +1,25 @@
+/**
+ * Copyright 2020 Quortex
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+variable "name" {
+  type        = string
+  description = "This value will be in the Name tag of all network resources."
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS region in wich to create network regional resources (subnet, router, nat...)."
+}
