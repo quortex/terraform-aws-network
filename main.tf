b10ec545dc2e90a5ccaef11bd79cfac1b59abfab
nerahou <nrahou@synamedia.com>
2025-01-07 11:27:22 +0100

    calculate newbits dynamically based on the number of availability zones

--- main.tf
+++ main.tf
@@ -16,6 +16,15 @@
 
 data "aws_region" "current" {}
 
+locals {
+  # Calculate newbits dynamically based on the number of availability zones
+  # 1 or 2 AZs, newbits = 1 (2 subnets needed).
+  # 3 or 4 AZs, newbits = 2 (4 subnets needed).
+  # 5 to 8 AZs, newbits = 3 (8 subnets needed).
+  # and so on...
+  newbits = ceil(log(length(var.availability_zones), 2))
+}
+
 # VPC
 resource "aws_vpc" "quortex" {
   cidr_block = var.vpc_cidr_block
@@ -39,7 +48,7 @@ resource "aws_default_security_group" "quortex" {
 }
 
 resource "aws_vpc_ipv4_cidr_block_association" "secondary" {
-  for_each   = toset([for index, az in var.availability_zones : cidrsubnet(var.vpc_secondary_cidr, 2, index)])
+  for_each   = toset([for index, az in var.availability_zones : cidrsubnet(var.vpc_secondary_cidr, local.newbits, index)])
   vpc_id     = aws_vpc.quortex.id
   cidr_block = each.value
 }
@@ -49,7 +58,7 @@ resource "aws_subnet" "quortex" {
     for key, subnet in var.subnets : {
       for index, az in var.availability_zones : "${key}-${data.aws_region.current.name}${az}" => {
         "availability_zone" = "${data.aws_region.current.name}${az}",
-        "cidr"              = cidrsubnet(subnet.cidr, 2, index),
+        "cidr"              = cidrsubnet(subnet.cidr, local.newbits, index),
         "public"            = subnet.public,
         "tags"              = subnet.tags,
       }
